77d60400ed9c981e5652d9185829c5d4b68d8f87
Peter Braun <peterkauft@gmx.de>
2021-10-02 17:34:34 +0200

    Added Prefab Variants for nice and easy pathing

--- Assets/Enemy/EnemyMover.cs
+++ Assets/Enemy/EnemyMover.cs
@@ -5,17 +5,18 @@ using UnityEngine;
 public class EnemyMover : MonoBehaviour
 {
     [SerializeField] List<Waypoint> path = new List<Waypoint>();
-    // Start is called before the first frame update
+    [SerializeField] float waitTime = 1f;
     void Start()
     {
-        PrintWaypointName();
+        StartCoroutine(FollowPath());
     }
 
-    void PrintWaypointName()
+    IEnumerator FollowPath()
     {
         foreach (Waypoint waypoint in path)
         {
-            Debug.Log(waypoint.name);
+            transform.position = waypoint.transform.position;
+            yield return new WaitForSeconds(waitTime); //Better then invoke -  After 1s continue - but still executes rest of start()
         }
     }
 }
